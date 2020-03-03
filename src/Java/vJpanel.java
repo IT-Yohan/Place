e24abeb87bc40dad4a02033f3c1270913627c0ae
IT-Yohan <IT-Yohan@users.noreply.github.com>
2020-03-03 23:53:57 +0100

    Ajout d'un Jpanel "?" Gautier faudra que tu vois ça t'es l'expert

---
+++ src/Java/vJpanel.java
@@ -0,0 +1,14 @@
+package Java;
+
+import javax.swing.JPanel;
+
+public class vJpanel extends JPanel {
+
+	/**
+	 * Je sais pas ce que je fais gautier t'es l'expert en panels j'ai pas suivi le cours 
+	 */
+	public vJpanel() {
+
+	}
+
+}
