ea844e8f06666f0280b2ff045886909b5633131f
chapuis <chapuis@SLAM-09.slam.sio>
2020-03-04 10:54:14 +0100

    iou

--- src/Java/vJpanel.java
+++ src/Java/vJpanel.java
@@ -5,7 +5,7 @@ import javax.swing.JPanel;
 public class vJpanel extends JPanel {
 
 	/**
-	 * Je sais pas ce que je fais gautier t'es l'expert en panels j'ai pas suivi le cours 
+	 * Je sais pas ce que je fais jgautier t'es l'expert en panels j'ai pas suivi le cours 
 	 */
 	public vJpanel() {
 
